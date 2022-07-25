34701f1655695a007602680693be7576e0149339
shiyongyong <shiyongyong@ezone.work>
2022-07-25 09:06:16 +0800

    remove  remark-directive dependency

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
 import { AtomList } from '@milkdown/utils';
 
 import { directiveContainerNode } from './containerDirective';
-import { remarkDirective } from './directive';
 import { directiveLeafNode } from './leafDirective';
 import { directiveTextNode } from './textDirective';
 
@@ -11,12 +10,10 @@ export const directiveFallback = AtomList.create([
     directiveContainerNode(),
     directiveTextNode(),
     directiveLeafNode(),
-    remarkDirective(),
 ]);
 
 export {
     directiveContainerNode,
     directiveTextNode,
     directiveLeafNode,
-    remarkDirective,
 }
